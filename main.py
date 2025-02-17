c905c9b0cf819fcbb0cf24d364ae5327db5709e0
maxxximgb <gabisovmaxim@gmail.com>
2025-02-17 19:31:20 +0300

    1

--- main.py
+++ main.py
@@ -4,26 +4,40 @@ import sys
 import requests
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
 import geocoder
 
 api_key = '7ab4d70e-469b-4d33-a528-0d04c7f43a9c'
-
+ll, lt = 30.0, 30.0
 
 class App(QWidget):
     def __init__(self):
         super().__init__()
+        self.setMouseTracking(True)
         self.pixmap = QPixmap()
         self.maplabel = QLabel()
         self.lout = QVBoxLayout()
         self.lout.addWidget(self.maplabel)
         self.setLayout(self.lout)
-        self.request_map('Владикавказ')
+        self.request_map(coords=(30, 30), span=(10, 10))
         self.show()
 
-    def request_map(self, coords=None, span=None, address=None):
+    def keyPressEvent(self, event):
+        global ll, lt
+        if event.key() == Qt.Key.Key_Down:
+            lt = lt - max(0, lt - self.size().width() // 100)
+        elif event.key() == Qt.Key.Key_Up:
+            lt += self.size().width() // 100
+        elif event.key() == Qt.Key.Key_Left:
+            lt = lt - max(0, lt - self.size().height() // 100)
+        elif event.key() == Qt.Key.Key_Right:
+            ll += self.size().height() // 100
 
+        self.request_map(coords=(ll, lt), span=(10, 10))
+
+    def request_map(self, coords=None, span=None, address=None):
         if coords and span:
-            ls = coords, span
+            ls = ",".join([str(e) for e in coords]), ",".join([str(float(e)) for e in span])
         else:
             ls = geocoder.get_ll_span(address)
 
